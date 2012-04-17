a14eeba1595b438ac41f0884c8212a290018aeef
Francesco Ferrara <fr.ferrara@gmail.com>
2012-04-17 17:31:18 +0200

    Continuing tests
    Signed-off-by: Francesco Ferrara <fr.ferrara@gmail.com>

--- main.cpp
+++ main.cpp
@@ -57,6 +57,49 @@ int main( int argc, char * argv[] ) {
 			  << ( circleParameters->get_xC() )( 1 ) \
 			  << ", r  = " << circleParameters->get_r() << std::endl;
 
+	x.resize( 2, n );
+	phi = 0.0;
+
+	// Generation
+	for ( int i = 0; i < n; i++ )
+	{
+		// Calculate the points
+		x( 0, i ) = x0 + r * cos( phi );
+		x( 1, i ) = y0 + r * sin( phi );
+
+		// Augment the angle
+		phi = phi + dphi;
+	}
+
+	circleParameters = circleDetector->detectCircle( x );
+
+	std::cout << "Detected circle:\n" \
+			  << "xC = " << ( circleParameters->get_xC() )( 0 ) << ", yC = " \
+			  << ( circleParameters->get_xC() )( 1 ) \
+			  << ", r  = " << circleParameters->get_r() << std::endl;
+
+	n = 1;
+	x.resize( 2, n );
+	phi = 0.0;
+
+	// Generation
+	for ( int i = 0; i < n; i++ )
+	{
+		// Calculate the points
+		x( 0, i ) = x0 + r * cos( phi );
+		x( 1, i ) = y0 + r * sin( phi );
+
+		// Augment the angle
+		phi = phi + dphi;
+	}
+
+	circleParameters = circleDetector->detectCircle( x );
+
+	std::cout << "Detected circle:\n" \
+			  << "xC = " << ( circleParameters->get_xC() )( 0 ) << ", yC = " \
+			  << ( circleParameters->get_xC() )( 1 ) \
+			  << ", r  = " << circleParameters->get_r() << std::endl;
+
 	return 0.0;
 }
 
