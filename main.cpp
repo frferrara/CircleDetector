b07310c8c79a978624ba9af27679fb279b5aabb0
Francesco Ferrara <fr.ferrara@gmail.com>
2012-04-19 17:30:26 +0200

    Continuing tests
    Signed-off-by: Francesco Ferrara <fr.ferrara@gmail.com>

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@
 
 
 int main() {
-	std::cout << "Test of circle detection!" << std::endl;
+	std::cout << "Test of circle detection!\n";
 
 	// Create a set of circle points
 	// Circle properties
@@ -57,6 +57,9 @@ int main() {
 			  << ( circleParameters->get_xC() )( 1 ) \
 			  << ", r  = " << circleParameters->get_r() << std::endl;
 
+	free( circleParameters );
+	circleParameters = new CircleParameters;
+
 	x.resize( 2, n );
 	phi = 0.0;
 
@@ -77,6 +80,9 @@ int main() {
 			  << "xC = " << ( circleParameters->get_xC() )( 0 ) << ", yC = " \
 			  << ( circleParameters->get_xC() )( 1 ) \
 			  << ", r  = " << circleParameters->get_r() << std::endl;
+
+	free( circleParameters );
+	circleParameters = new CircleParameters;
 
 	n = 1;
 	x.resize( 2, n );
@@ -99,6 +105,9 @@ int main() {
 			  << "xC = " << ( circleParameters->get_xC() )( 0 ) << ", yC = " \
 			  << ( circleParameters->get_xC() )( 1 ) \
 			  << ", r  = " << circleParameters->get_r() << std::endl;
+
+	free( circleParameters );
+	circleParameters = new CircleParameters;
 
 	n = 1;
 	x.resize( n, 2 );
@@ -122,6 +131,9 @@ int main() {
 			  << ( circleParameters->get_xC() )( 1 ) \
 			  << ", r  = " << circleParameters->get_r() << std::endl;
 
+	free( circleParameters );
+	circleParameters = new CircleParameters;
+
 	n = 4;
 	x.resize( n, 3 );
 	phi = 0.0;
@@ -144,6 +156,9 @@ int main() {
 			  << ( circleParameters->get_xC() )( 1 ) \
 			  << ", r  = " << circleParameters->get_r() << std::endl;
 
+	free( circleParameters );
+	circleParameters = new CircleParameters;
+
 	n = 4;
 	x.resize( 3, n );
 	phi = 0.0;
@@ -165,6 +180,79 @@ int main() {
 			  << "xC = " << ( circleParameters->get_xC() )( 0 ) << ", yC = " \
 			  << ( circleParameters->get_xC() )( 1 ) \
 			  << ", r  = " << circleParameters->get_r() << std::endl;
+
+	free( circleParameters );
+	circleParameters = new CircleParameters;
+
+	gsl_histogram2d * hist_xC = gsl_histogram2d_calloc_uniform( 10, 10, \
+																0.0, 10.0, \
+																0.0, 10.0 );
+	gsl_histogram * hist_r = gsl_histogram_calloc_uniform( 10, 0.0, 10.0 );
+
+	unsigned int j = 27, n_ = 100000, numPoints = 5;
+
+	n = round( 2 * M_PI / dphi ) + 1;
+	x.resize( n, 2 );
+	phi = 0.0;
+
+	// Generation
+	for ( int i = 0; i < n; i++ )
+	{
+		// Calculate the points
+		x( i, 0 ) = x0 + r * cos( phi );
+		x( i, 1 ) = y0 + r * sin( phi );
+
+		// Augment the angle
+		phi = phi + dphi;
+	}
+
+	try {
+		circleDetector = new CircleDetector( \
+				new TBBLSCircDet( 5, hist_xC, hist_r, j, n_ ) );
+
+		circleParameters = circleDetector->detectCircle( x );
+	} catch ( std::runtime_error & e ) {
+		std::cout << std::endl << e.what() << std::endl;
+	}
+
+	std::cout << "Detected circle:\n" \
+			  << "xC = " << ( circleParameters->get_xC() )( 0 ) << ", yC = " \
+			  << ( circleParameters->get_xC() )( 1 ) \
+			  << ", r  = " << circleParameters->get_r() << std::endl;
+
+	free( circleParameters );
+	circleParameters = new CircleParameters;
+
+	x.resize( 2, n );
+	phi = 0.0;
+
+	// Generation
+	for ( int i = 0; i < n; i++ )
+	{
+		// Calculate the points
+		x( 0, i ) = x0 + r * cos( phi );
+		x( 1, i ) = y0 + r * sin( phi );
+
+		// Augment the angle
+		phi = phi + dphi;
+	}
+
+	try {
+		circleDetector = new CircleDetector( \
+				new TBBLSCircDet( 5, hist_xC, hist_r, j, n_ ) );
+
+		circleParameters = circleDetector->detectCircle( x );
+	} catch ( std::runtime_error & e ) {
+		std::cout << std::endl << e.what() << std::endl;
+	}
+
+	std::cout << "Detected circle:\n" \
+			  << "xC = " << ( circleParameters->get_xC() )( 0 ) << ", yC = " \
+			  << ( circleParameters->get_xC() )( 1 ) \
+			  << ", r  = " << circleParameters->get_r() << std::endl;
+
+	free( circleParameters );
+	circleParameters = new CircleParameters;
 
 	return 0.0;
 }
